554d069658fd4b98a783a1c36b7125ee1593e872
kgykgy10 <117807115+kgykgy10@users.noreply.github.com>
2022-12-20 22:35:16 +0900

    청약홈 당첨자 선정
    아파트 별 당첨컷 설정해서 
    당첨자 선정

--- kgy_project/src/kgy_project/Person.java
+++ kgy_project/src/kgy_project/Person.java
@@ -1,45 +1,29 @@
 package kgy_project;
 
-import java.util.ArrayList;
-
-public class Person {
-	private int personId;
-	private String personName;
-	
-	private ArrayList<Score> scoreList =  new ArrayList<Score>(); // 청약자의 청약점수 리스트
-	
-		public Person(int personId, String personName) {
-		super();
-		this.personId = personId;
-		this.personName = personName;
-	}
-
-		public void addScore(Score score) {
-			scoreList.add(score); // ********* 청약자의 청약점수를 등록하는 메서드
-		}
+//이 클래스에서는 청약자를 정의.
+public class Person 
+{
+		private int personId;
+		private String personName;
+		private int score;
 		
+	    // 청약자 생성(이슈: Person class(6-8 line)과 아래 매개변수를 같게 썼다가 오류나서 변경)
+		public Person(int pId, String pName, int pScore) {
+		super();
+		this.personId = pId;
+		this.personName = pName;
+		this.score = pScore;
+		}
+		// 청약자Id를 가져오는 함수
 		public int getPersonId() {
 			return personId;
 		}
-
-		public void setPersonId(int personId) {
-			this.personId = personId;
-		}
-
+		// 청약자이름을 가져오는 함수
 		public String getPersonName() {
 			return personName;
 		}
-
-		public void setPersonName(String personName) {
-			this.personName = personName;
+		// 청약점수를 가져오는 함수
+		public int getPersonScore() {
+			return score;
 		}
-
-		public ArrayList<Score> getScoreList() {
-			return scoreList;            // 청약자 점수 목록 조회
-		}
-
-		public void setScoreList(ArrayList<Score> scoreList) {
-			this.scoreList = scoreList;
-		}
-		
 }
